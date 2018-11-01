dbfb209c6e09ed525e10d7d11fdd2b5ca98c463f
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-01 14:24:09 -0400

    add spacetime for dart

--- dart/command.sh
+++ dart/command.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# OCAML_SPACETIME_INTERVAL=20000 ~/rooibos-future/main -sequential -verbose -match-timeout 2 -d $(pwd) -f .dart -templates `echo ~/rooibos-future/catalogue/dart/dart_style/use-is-not-empty | tr ' ' ,` > x-statistics.teresy 
+# ~/prof_spacetime/_build/default/bin/prof_spacetime.exe serve --address=0.0.0.0 spacetime-56484
