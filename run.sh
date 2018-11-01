1627f031db513d994b2fc187a8eb912e9795a15f
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-01 18:22:09 +0000

    initial

---
+++ run.sh
@@ -0,0 +1,3 @@
+OCAML_SPACETIME_INTERVAL=10000 ~/rooibos-future/main -sequential -verbose -match-timeout 2 -d $(pwd) -f .go -templates `echo ~/rooibos-future/catalogue/go/staticcheck/redundant-nil-check-loop-s1031 | tr ' ' ,` > statistics.teresy 
+# prof_spacetime view spacetime-XXX
+# prof_spacetime process spacetime-XXX
